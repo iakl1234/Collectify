83373454c7598372340ca3fa9c446dc6a9f1e3db
bubsisy <83180384+bubsisy@users.noreply.github.com>
2025-03-13 10:18:36 +0300

    bug fixxed

--- Assets/Resources/Scripts/FirestoreManager.cs
+++ Assets/Resources/Scripts/FirestoreManager.cs
@@ -1,28 +1,34 @@
 using Firebase;
+using Firebase.Auth;
+using Firebase.Extensions;
 using Firebase.Firestore;
 using UnityEngine;
 
 public class FirestoreManager : MonoBehaviour
 {
-   
-    //const string url = "https://collectorsapp-9330a.firestore.app";
-    //public static FirestoreManager Instance;
-    //void Awake()
-    //{
-    //    Instance = this;
-       
-    //    FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
-    //    {
-    //        Debug.Log("ff");
-    //        if (task.IsFaulted)
-    //        {
-    //            Debug.LogError("Firebase initialization failed: " + task.Exception);
-    //        }
-    //        else if (task.IsCompleted)
-    //        {
-    //            Debug.LogError("its work");
-    //        }
-    //    });
-    //}
+
+    const string url = "https://collectorsapp-9330a.firestore.app";
+    private FirebaseAuth auth;
+    public static FirestoreManager Instance;
+    private FirebaseFirestore firestore;
+    void Awake()
+    {
+        Instance = this;
+        firestore = FirebaseFirestore.DefaultInstance;
+        auth = FirebaseAuth.DefaultInstance;
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            Debug.Log("ff");
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Firebase initialization failed: " + task.Exception);
+            }
+            else if (task.IsCompleted)
+            {
+                Debug.Log("its work");
+            }
+        });
+    }
+
 
 }
